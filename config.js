63be6f1c51ea1b7fba321cdd13566c2a992324af
rambo1z <104389490+rambo1z@users.noreply.github.com>
2022-10-02 00:57:57 +0300

    Update config.js

--- config.js
+++ config.js
@@ -109,10 +109,10 @@ global.APIKeys = { // APIKey Here
 // Kata APIKEY itu isi dengan apikey lu sendiri, dengan beli ke website itu
 
 /*============== WATERMARK ==============*/
-global.wm = '                「 WH-MODS-BOT-V1 」' //Main Watermark
+global.wm = '                「 اينو بوت」' //Main Watermark
 global.wm2 = '꒷︶꒷꒥꒷ ‧₊˚ ꒰ฅ˘WH-MODS-BOT-V1 ˘ฅ ꒱ ‧₊˚꒷︶꒷꒥꒷'
-global.wm3 = '⫹⫺ WH-MODS-BOT-V1 '
-global.botdate = `⫹⫺ DATE: ${week} ${date}\n⫹⫺ 𝗧𝗶𝗺𝗲: ${wktuwib}`
+global.wm3 = '⫹⫺ 2-v-ايـنـو '
+global.botdate = `⫹⫺ التاريخ: ${week} ${date}\n⫹⫺ الوقت: ${wktuwib}`
 global.bottime = `T I M E : ${wktuwib}`
 global.titlebot = `⫹⫺ WhatsApp Bot | By WH-MODS-DEV`
 global.author = '                「 WH-MODS-BOT-V1  あ」'
