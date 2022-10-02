6d11a1ec7957ed49a7fc490399249ddcce3d84b5
rambo1z <104389490+rambo1z@users.noreply.github.com>
2022-10-02 03:46:58 +0300

    Update xp-profile.js

--- plugins/xp-profile.js
+++ plugins/xp-profile.js
@@ -10,17 +10,14 @@ let handler = async (m, { conn }) => {
     let name = conn.getName(who)
     let fkon = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
     let str = `
-]──────────❏ *البروفايل* ❏──────────[
+]──────❏ *البروفايل* ❏──────[
 💌 • *الاسم:* ${username} 
 🎐 • *:* ${registered ? name : ''}
 📧 • *المنشن:* @${who.replace(/@.+/, '')}
 📞 • *الرقم:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
 🔗 • *الرابط:* https://wa.me/${who.split`@`[0]}
 🎨 • *العمر:* ${registered ? age : ''}
-${readMore}
-🌟 • *Premium:* ${premium ? "✅" :"❌"}
-⏰ • *PremiumTime:* 
-${clockString(user.premiumTime)}
+
 📑 • *التسجيل:* ${registered ? '✅': '❌'}
 `.trim()
     conn.sendButton(m.chat, str, botdate, pp, [[`${registered ? 'Menu':'Verify'}`, `${user.registered ? '.menu':'.verify'}`]], fkon, { contextInfo: { mentionedJid: [who], forwardingScore: 999, isForwarded: true}})
@@ -38,5 +35,4 @@ function clockString(ms) {
   let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [d, ' *Days ☀️*\n ', h, ' *Hours 🕐*\n ', m, ' *Minute ⏰*\n ', s, ' *Second ⏱️* '].map(v => v.toString().padStart(2, 0)).join('')
-}
+ 
