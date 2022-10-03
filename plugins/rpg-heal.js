eaa47ff191ebf342e3f098328afbef31348119db
rambo1z <104389490+rambo1z@users.noreply.github.com>
2022-10-04 01:16:34 +0300

    Update rpg-heal.js

--- plugins/rpg-heal.js
+++ plugins/rpg-heal.js
@@ -1,20 +1,20 @@
 let handler = async (m, { args, usedPrefix }) => {
     let user = global.db.data.users[m.sender]
     if (user.health >= 100) return m.reply(`
- ممتلئة❤️صحتك!
+ صحتك❤️ممتلئة!
 `.trim())
 let buf = user.cat
 let buff = (buf == 0 ? '5' : '' || buf == 1 ? '10' : '' || buf == 2 ? '15' : '' || buf == 3 ? '20' : '' || buf == 4 ? '25' : '' || buf == 5 ? '30' : '' || buf == 6 ? '35' : '' || buf == 7 ? '40' : '' || buf == 8 ? '45' : '' || buf == 9 ? '50' : '' || buf == 10 ? '100' : '' || buf == 11 ? '100' : '') 
     const heal = 15 + (buff * 4)
     let count = Math.max(1, Math.min(Number.MAX_SAFE_INTEGER, (isNumber(args[0]) && parseInt(args[0]) || Math.round((100 - user.health) / heal)))) * 1
     if (user.potion < count) return m.reply(`
- انت لديك فقط,🥤جرعتك غير كافية  *${user.potion}* 🥤جرعة
-type *${usedPrefix}buy potion ${count - user.potion}* to buy 🥤جرعة
+ جرعتك غير كافية. انت لديك فقط  *${user.potion}* 🥤جرعة
+اكتب *${usedPrefix}buy potion ${count - user.potion}*  جرعة🥤لشراء
 `.trim())
     user.potion -= count * 1
     user.health += heal * count
     m.reply(`
-لقد نجحت باستخدام الـ *${count}* 🥤جرعة(s)
+لقد نجحت باستخدام الـ *${count}* 🥤جرعة
 `.trim())
 }
 
