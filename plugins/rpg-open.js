8640e2af554d24444fd107d3cc40ad2a56406323
rambo1z <104389490+rambo1z@users.noreply.github.com>
2022-10-04 03:05:48 +0300

    Update rpg-open.js

--- plugins/rpg-open.js
+++ plugins/rpg-open.js
@@ -2,59 +2,59 @@ const rewards = {
     common: {
         xفلوس: 101,
         xاكس_بيx: 201,
-        xهx: 11,
-        xx: [0, 1, 0, 1, 0, 0, 0, 0, 0],
-        xx: [0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
-        xx: [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        xزبالةx: 11,
+        xجرعةx: [0, 1, 0, 1, 0, 0, 0, 0, 0],
+        xصندوقx: [0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
+        xغير_معرفx: [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     },
     uncommon: {
-        money: 201,
-        exp: 401,
-        trash: 31,
-        potion: [0, 1, 0, 0, 0, 0, 0],
-        diamond: [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        common: [0, 1, 0, 0, 0, 0, 0, 0],
-        uncommon: [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-        mythic: [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        wood: [0, 1, 0, 0, 0, 0],
-        rock: [0, 1, 0, 0, 0, 0],
-        string: [0, 1, 0, 0, 0, 0]
+        xزبالةx: 201,
+        xاكس_بيx: 401,
+        xزبالةx: 31,
+        xجرعةx: [0, 1, 0, 0, 0, 0, 0],
+        xالماسx: [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        xصندوقx: [0, 1, 0, 0, 0, 0, 0, 0],
+        xغير_معرفx: [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        xنادرx: [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        xخشبx: [0, 1, 0, 0, 0, 0],
+        xحجرx: [0, 1, 0, 0, 0, 0],
+        xخيطx: [0, 1, 0, 0, 0, 0]
     },
     mythic: {
-        money: 301,
-        exp: 551,
-        trash: 61,
-        potion: [0, 1, 0, 0, 0, 0],
-        emerald: [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        diamond: [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-        gold: [0, 1, 0, 0, 0, 0, 0, 0, 0],
-        iron: [0, 1, 0, 0, 0, 0, 0, 0],
-        common: [0, 1, 0, 0, 0, 0],
-        uncommon: [0, 1, 0, 0, 0, 0, 0, 0],
-        mythic: [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-        legendary: [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        pet: [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        wood: [0, 1, 0, 0, 0],
-        rock: [0, 1, 0, 0, 0],
-        string: [0, 1, 0, 0, 0]
+        xفلوسx: 301,
+        xاكس_بيx: 551,
+        xزبالةx: 61,
+        xجرعةx: [0, 1, 0, 0, 0, 0],
+        xزمردx: [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        xالماسx: [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        xذهبx: [0, 1, 0, 0, 0, 0, 0, 0, 0],
+        xحديدx: [0, 1, 0, 0, 0, 0, 0, 0],
+        xصندوقx: [0, 1, 0, 0, 0, 0],
+        xغير_معرفx: [0, 1, 0, 0, 0, 0, 0, 0],
+        xنادرx: [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        xاسطوريx: [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        xحيوانx: [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        xخشبx: [0, 1, 0, 0, 0],
+        xحجرx: [0, 1, 0, 0, 0],
+        xخيطx: [0, 1, 0, 0, 0]
     },
     legendary: {
-        money: 401,
-        exp: 601,
-        trash: 101,
-        potion: [0, 1, 0, 0, 0],
-        emerald: [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-        diamond: [0, 1, 0, 0, 0, 0, 0, 0, 0],
-        gold: [0, 1, 0, 0, 0, 0, 0, 0],
-        iron: [0, 1, 0, 0, 0, 0, 0],
-        common: [0, 1, 0, 0],
-        uncommon: [0, 1, 0, 0, 0, 0],
-        mythic: [0, 1, 0, 0, 0, 0, 0, 0, 0],
-        legendary: [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-        pet: [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-        wood: [0, 1, 0, 0],
-        rock: [0, 1, 0, 0],
-        string: [0, 1, 0, 0]
+        xفلوسx: 401,
+        xاكس_بيx: 601,
+        xزبالةx: 101,
+        xجرعةx: [0, 1, 0, 0, 0],
+        xزمردx: [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        xالماسx: [0, 1, 0, 0, 0, 0, 0, 0, 0],
+        xذهبx: [0, 1, 0, 0, 0, 0, 0, 0],
+        xحديدx: [0, 1, 0, 0, 0, 0, 0],
+        xصندوقx: [0, 1, 0, 0],
+        xغير_معرفx: [0, 1, 0, 0, 0, 0],
+        xنادرx: [0, 1, 0, 0, 0, 0, 0, 0, 0],
+        xاسطوريx: [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        xحيوانx: [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        xخشبx: [0, 1, 0, 0],
+        xحجرx: [0, 1, 0, 0],
+        xخيطx: [0, 1, 0, 0]
     },
     // pet: {
     //     petFood: [0, 1, 0, 0, 0],
@@ -77,8 +77,8 @@ ${rpg.emoticon(v)}${v}
     let count = Math.floor(isNumber(args[1]) ? Math.min(Math.max(parseInt(args[1]), 1), Number.MAX_SAFE_INTEGER) : 1) * 1
     if (!(type in listCrate)) return m.reply(info)
     if (user[type] < count) return m.reply(`
-Your *${rpg.emoticon(type)}${type} crate* is not enough!, you only have ${user[type]} *${rpg.emoticon(type)}${type} crate*
-type *${usedPrefix}buy ${type} ${count - user[type]}* to buy
+الخاص بك *${rpg.emoticon(type)}${type} crate* لايكفي. انت اديك فقط ${user[type]} *${rpg.emoticon(type)}${type} crate*
+اكتب *${usedPrefix}buy ${type} ${count - user[type]}* للشراء
 `.trim())
     // TODO: add pet crate
     // if (type !== 'pet')
@@ -94,17 +94,17 @@ type *${usedPrefix}buy ${type} ${count - user[type]}* to buy
             }
     user[type] -= count * 1
     m.reply(`
-You have opened *${count}* ${global.rpg.emoticon(type)}${type} crate and got:
+لقد فتحت *${count}* ${global.rpg.emoticon(type)}${type} وحصلت على:
 ${Object.keys(crateReward).filter(v => v && crateReward[v] && !/legendary|pet|mythic|diamond|emerald/i.test(v)).map(reward => `
 *${global.rpg.emoticon(reward)}${reward}:* ${crateReward[reward]}
 `.trim()).join('\n')}
 `.trim())
     let diamond = crateReward.diamond, mythic = crateReward.mythic, pet = crateReward.pet, legendary = crateReward.legendary, emerald = crateReward.emerald
     if (mythic || diamond) m.reply(`
-Congrats you got a rare item, which is ${diamond ? `*${diamond}* ${rpg.emoticon('diamond')}diamond` : ''}${diamond && mythic ? 'and ' : ''}${mythic ? `*${mythic}* ${rpg.emoticon('mythic')}mythic` : ''}
+تهانينا لقد حصلت على عنصر نادر ${diamond ? `*${diamond}* ${rpg.emoticon('diamond')}diamond` : ''}${diamond && mythic ? 'and ' : ''}${mythic ? `*${mythic}* ${rpg.emoticon('mythic')}mythic` : ''}
 `.trim())
     if (pet || legendary || emerald) m.reply(`
-Congrats you got a epic item, which is ${pet ? `*${pet}* ${rpg.emoticon('pet')}pet` : ''}${pet && legendary && emerald ? ', ' : (pet && legendary || legendary && emerald || emerald && pet) ? 'and ' : ''}${legendary ? `*${legendary}* ${rpg.emoticon('legendary')}legendary` : ''}${pet && legendary && emerald ? 'and ' : ''}${emerald ? `*${emerald}* ${rpg.emoticon('emerald')}emerald` : ''}
+تهانينا لقد حصلت على عنصر ملحمي ${pet ? `*${pet}* ${rpg.emoticon('pet')}pet` : ''}${pet && legendary && emerald ? ', ' : (pet && legendary || legendary && emerald || emerald && pet) ? 'and ' : ''}${legendary ? `*${legendary}* ${rpg.emoticon('legendary')}legendary` : ''}${pet && legendary && emerald ? 'and ' : ''}${emerald ? `*${emerald}* ${rpg.emoticon('emerald')}emerald` : ''}
 `.trim())
 }
 handler.help = ['open', 'gacha'].map(v => v + ' [crate] [count]')
