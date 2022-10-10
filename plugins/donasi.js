243ea236e9ae5dcaf3c2b9a5654ab4ef917ab295
rambo1z <104389490+rambo1z@users.noreply.github.com>
2022-10-10 17:51:55 +0300

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -2,6 +2,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
 let text = `
 ┌─「قروب  • الدعم 」
 │ • *الرابط:https://chat.whatsapp.com/L30GtP3peP6FfBzWk5MBdw* 
+│ • *http://wa.me/9647735131102?text=*السلام🤍عليكم*
 ❏────
 `.trim()
   m.reply(text)
