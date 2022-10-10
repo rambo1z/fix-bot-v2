fe53cbfaf2318f7ac336d37d2a146c244edcd8bf
rambo1z <104389490+rambo1z@users.noreply.github.com>
2022-10-10 12:13:09 +0300

    Update config.js

--- config.js
+++ config.js
@@ -30,11 +30,11 @@ let wibh = moment.tz('Asia/Jakarta').format('HH')
 /*============== SOCIAL ==============*/
 
 // kalo ga punya ketik "-" atau biarin aja biar ada creditsnya :v
-global.sig = 'https://instagram.com/_ctzhid'
+global.sig = 'http://wa.me/9647735131102?text=*%D8%A7%D9%84%D8%B3%D9%84%D8%A7%D9%85%F0%9F%A4%8D%D8%B9%D9%84%D9%8A%D9%83%D9%85'
 global.sgh = '-' //github
-global.sgc = 'https://chat.whatsapp.com/Iu9I1k8FnXW1sXT41XIF50' //group whatsapp
+global.sgc = 'https://chat.whatsapp.com/L30GtP3peP6FfBzWk5MBdw' //group whatsapp
 global.sdc = '-' //discord
-global.snh = 'https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow' // my youtube channel
+global.snh = 'http://wa.me/9647735131102?text=*%D8%A7%D9%84%D8%B3%D9%84%D8%A7%D9%85%F0%9F%A4%8D%D8%B9%D9%84%D9%8A%D9%83%D9%85' // my youtube channel
 
 /*============== PAYMENT ==============*/
 global.pdana = '08XXXXXXXXXX' //dana
